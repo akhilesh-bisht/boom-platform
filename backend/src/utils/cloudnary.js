498d05d9d4d1c86ced39c1bb913edefc38442551
Akhilesh Bisht <150502170+akhilesh-bisht@users.noreply.github.com>
2025-05-25 23:53:11 +0530

    - Implement multer , - Add cloudinary - Define ApiError and ApiResponse classes for standardized error and response handling.

---
+++ backend/src/utils/cloudnary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (filePath) => {
+  try {
+    if (!filePath) return null;
+
+    // upload the file to cloudinary
+    const response = await cloudinary.uploader.upload(filePath, {
+      resource_type: "auto",
+    });
+    console.log("Cloudinary upload response:", response);
+    fs.unlinkSync(filePath);
+    return response;
+  } catch (err) {
+    fs.unlinkSync(filePath); // remove the file from locally
+    console.log(`Error uploading file to cloudinary: ${err.message}`, err);
+    throw new Error(`Error uploading file to cloudinary: ${err.message}`);
+  }
+};
+
+export { uploadOnCloudinary };
